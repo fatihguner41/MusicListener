ea9914245baf69036997929f46cbb0d347fdbcbe
fatihguner41 <fatihguner7@gmail.com>
2023-07-09 21:08:45 +0300

    Project finished

---
+++ proje 2.3/src/Database.java
@@ -0,0 +1,16 @@
+
+
+public class Database {
+    
+    public static final String kullanici_adi="root";
+    public static final String parola="123321fg";
+    
+    public static final String db_ismi="spofity";
+    public  static final String host="localhost";
+    public static final int port=3306;
+    
+  
+   
+    
+    
+}
